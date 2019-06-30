5124e5873d471b7eb5e5f8e5652381dec6b290bd
agares <me@agares.info>
2019-07-01 00:16:41 +0200

    Cleanup MediaPlayer

--- src/blocks/media_player.rs
+++ src/blocks/media_player.rs
@@ -74,6 +74,35 @@ fn find_player<'a>() -> Result<Player<'a>, PlayerFindingError> {
     Ok(player)
 }
 
+#[derive(Debug)]
+struct PlayerError(String);
+
+impl From<DBusError> for PlayerError {
+    fn from(err: DBusError) -> Self {
+        PlayerError(format!("{:?}", err))
+    }
+}
+
+fn get_artist_and_title(player:&Player) -> Result<Option<(String, String)>, PlayerError> {
+    let metadata = player.get_metadata()?;
+    let artists;
+    let title;
+
+    if let Some(artists_vec) = metadata.artists() {
+        artists = artists_vec.join(", ");
+    } else {
+        return Ok(None);
+    }
+
+    if let Some(title_opt) = metadata.title() {
+        title = title_opt.into();
+    } else {
+        return Ok(None);
+    }
+
+    Ok(Some((artists, title)))
+}
+
 impl MediaPlayer {
     pub fn new() -> Self {
         let (command_sender, command_receiver): (
@@ -90,43 +119,35 @@ impl MediaPlayer {
             thread: Some(thread::spawn(move || 'mainloop: loop {
                 let message = command_receiver.recv_timeout(Duration::from_millis(500));
 
-                let should_continue = find_player()
+                let should_continue:Result<bool, PlayerError> = find_player()
                     .map(|player| {
-                        let metadata = player
-                            .get_metadata()
-                            .or_else(|_| Err(BlockError::new("".into())));
+                        let artist_and_title = get_artist_and_title(&player)?;
 
-                        metadata
-                            .map(|metadata| {
-                                let artist = metadata.artists().and_then(|artists| artists.first());
-
-                                let title = metadata.title();
-
-                                if let (Some(artist), Some(title)) = (artist, title) {
-                                    state_sender
-                                        .send(MediaPlayerStateChange::NowPlaying {
-                                            artist: artist.clone(),
-                                            title: title.into(),
-                                        })
-                                        .unwrap();
-                                }
-                            })
-                            .unwrap();
+                        if let Some((artist, title)) = artist_and_title {
+                            state_sender
+                                .send(MediaPlayerStateChange::NowPlaying {
+                                    artist,
+                                    title,
+                                })
+                                .unwrap();
+                        }
 
                         match message {
-                            Result::Ok(MediaPlayerRequest::Quit) => false,
+                            Result::Ok(MediaPlayerRequest::Quit) => Ok(false),
                             Result::Ok(MediaPlayerRequest::TogglePause) => {
                                 player.play_pause().unwrap();
-                                true
+                                Ok(true)
                             }
-                            Result::Err(_) => true,
+                            Result::Err(_) => Ok(true),
                         }
                     })
-                    .unwrap_or(true);
+                    .unwrap_or(Ok(true));
 
-                if !should_continue {
+                if let Ok(false) = should_continue {
                     break 'mainloop;
                 }
+
+                // fixme log error
             })),
             command_sender,
             state_receiver,
